ba56906c99b6e6c4d8e7d9bd42d81d10e0ecc23e
Drake Williamson <williamson.drake@outlook.com>
2024-03-21 14:06:42 -0700

    feat: create division function to pass its corresponding test

--- src/functions/calculator.js
+++ src/functions/calculator.js
@@ -1,6 +1,7 @@
 const calculator = {
   add: (input) => input[0] + input[1],
   subtract: (input) => input[0] - input[1],
+  divide: (input) => input[0] / input[1],
 };
 
 export default calculator;
