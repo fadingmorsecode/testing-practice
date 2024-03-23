7a552ab64bcda347a6036ca236e9c2b9dea4a786
Drake Williamson <williamson.drake@outlook.com>
2024-03-22 23:51:29 -0700

    feat: write function to pass min test

--- src/functions/analyzeArray.js
+++ src/functions/analyzeArray.js
@@ -6,9 +6,13 @@ function getAverage(arr) {
   return average / arr.length;
 }
 
+function getMin(arr) {
+  return Math.min(...arr);
+}
+
 export default function analyzeArray(arr) {
   const analyzedObj = {};
-  const averageOfArr = getAverage(arr);
-  analyzedObj["average"] = averageOfArr;
+  analyzedObj.average = getAverage(arr);
+  analyzedObj.min = getMin(arr);
   return analyzedObj;
 }
