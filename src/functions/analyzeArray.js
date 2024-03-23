0aa93408fac4529d8f01c04605770fd3a50eb68a
Drake Williamson <williamson.drake@outlook.com>
2024-03-22 23:55:22 -0700

    feat: create function to pass max test

--- src/functions/analyzeArray.js
+++ src/functions/analyzeArray.js
@@ -10,9 +10,14 @@ function getMin(arr) {
   return Math.min(...arr);
 }
 
+function getMax(arr) {
+  return Math.max(...arr);
+}
+
 export default function analyzeArray(arr) {
   const analyzedObj = {};
   analyzedObj.average = getAverage(arr);
   analyzedObj.min = getMin(arr);
+  analyzedObj.max = getMax(arr);
   return analyzedObj;
 }
